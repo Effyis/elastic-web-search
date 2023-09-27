6491ccdac854a0d1318bfbe7f13ede8996938f26
gianeste <105244270+gianeste@users.noreply.github.com>
2023-09-27 19:49:07 -0400

    JSON logic rewrite

--- search.py
+++ search.py
@@ -12,19 +12,12 @@ es = Elasticsearch(
 
 @app.route('/')
 def index():
-    # Retrieve list of indices from Elasticsearch using the _cat API
-    indices = es.cat.indices(format="json")
-
-    # Extract the index names from the response
-    index_names = [index_info['index'] for index_info in indices]
-
-    return render_template('index.html', indices=index_names)
+    return render_template('index.html')
 
 
 @app.route('/search', methods=['POST'])
 def search():
-    index_pattern = request.form.get(
-        'index_name') or request.form.get('index_dropdown')
+    index_pattern = "boards-crawl-logs-*"
     query = request.form.get('query')
 
     # Construct the search body based on the query
@@ -73,26 +66,18 @@ def search():
 
     return render_template('results.html', results=processed_results, original_url=original_url, query=query, index=index_pattern)
 
+
 def hyperlink_urls_in_dict(d, query):
-    # Pattern to match URLs and file paths
-    url_pattern = re.compile(
-        r'(?:(?:http[s]?://)?(?:(?:[a-zA-Z0-9-]+)\.)+[a-zA-Z]{2,6}[\w/._-]*(?:\?[^\s]*)?)|(/[\w/._-]+)'
-    )
-    
     for key, value in d.items():
         if isinstance(value, dict):
             hyperlink_urls_in_dict(value, query)
         elif isinstance(value, str):
-            # Create the hyperlink for matched patterns
-            def repl(match):
-                url = match.group(0)
-                # If it's a file path, replace text with "view file"
-                if url.startswith('/'):
-                    return f'<a href="{url}" target="_blank">view file</a>'
-                return f'<a href="{url}" target="_blank">{url}</a>'
-            
-            value = url_pattern.sub(repl, value)
-            
+            # Handle special cases for certain keys
+            if key == 'siteUrl' and not value.startswith(('http://', 'https://')):
+                value = 'http://' + value
+            elif key == 'logFileName':
+                value = f'<a href="{value}" target="_blank">View Log File</a>'
+
             # Split the string using '<a' and '</a>' as delimiters
             # Only apply highlighting outside of these delimiters to avoid corrupting anchor tags
             parts = re.split(r'(<a|</a>)', value)
@@ -100,10 +85,11 @@ def hyperlink_urls_in_dict(d, query):
                 # If the part is not inside an anchor tag, apply highlighting
                 if i % 3 == 0:
                     parts[i] = re.sub(re.escape(query), r'<span style="background-color: #ADD8E6">\g<0></span>', part, flags=re.IGNORECASE)
-            
+
             d[key] = ''.join(parts)
 
     return d
+
 
 if __name__ == "__main__":
     app.run(debug=True)
